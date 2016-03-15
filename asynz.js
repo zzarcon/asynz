feb326fd957d476f8c1208a125bd7f6ec3801c3d
Hector Leon Zarco Garcia <hezarco@gmail.com>
2016-03-15 21:57:37 +0100

    test setup

--- asynz.js
+++ asynz.js
@@ -1,3 +1,27 @@
-export default {
-  foo: 'bar'
-};  
+let cache = {};
+
+function onload(src, resolve) {
+  cache[src] = true;
+
+  return resolve;
+}
+
+export default (src, attrs = {}) => {
+  return new Promise((resolve) => {
+    const isAlreadyLoaded = cache[src];
+    if (isAlreadyLoaded) return resolve();
+
+    let script = document.createElement('script');
+
+    script.type = 'text/javascript';
+    script.src = src;
+
+    Object.keys(attrs).forEach(key => {
+      script.setAttribute(key, attrs[key]);
+    });
+
+    script.onload = onload(src, resolve);
+
+    document.body.appendChild(script);
+  });
+};
--- test.js
+++ test.js
@@ -1,8 +1,42 @@
 import asynz from './asynz';
-import test from 'tape';
+import tapeTest from 'tape';
 
-test('A passing test', (assert) => {
-  assert.pass(asynz);
+function before() {
+  delete window.jQuery;
 
-  assert.end();
+  let scripts = [...document.querySelectorAll('body script')];
+  
+  scripts.forEach(s => s.remove());
+};
+
+
+function test(title, cb) {
+  tapeTest(title, (...args) => {
+    before();
+    cb(...args);
+  });
+}
+
+test('Loading jquery', (assert) => {
+  const src = 'https://code.jquery.com/jquery-2.2.1.js';
+
+  assert.pass(!window.jQuery);
+
+  asynz(src).then(() => {
+    assert.pass(!!window.jQuery);
+
+    assert.end();
+  });
+});
+
+test('Cache: loads the script for the first time', () => {
+  console.log(window.jQuery);
+});
+
+test('Cache: dont load the script if was already loaded', () => {
+
+});
+
+test('Add the proper attributes to the script', () => {
+
 });
