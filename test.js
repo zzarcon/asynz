7314c2eed0b0fb1134a14f59c1d1a906053ec7f3
Hector Leon Zarco Garcia <hezarco@gmail.com>
2016-03-18 17:39:27 +0100

    Testing the cache

--- test.js
+++ test.js
@@ -1,14 +1,54 @@
 import asynz from './asynz';
 import tapeTest from 'tape';
 
+const src = 'https://code.jquery.com/jquery-2.2.1.js';
+
 function before() {
   delete window.jQuery;
 
-  let scripts = [...document.querySelectorAll('body script')];
+  let scripts = [...scriptsNumber()];
   
   scripts.forEach(s => s.remove());
 };
 
+test('Loading jquery', assert => {
+  assert.pass(!window.jQuery);
+
+  asynz(src).then(() => {
+    assert.pass(!!window.jQuery);
+    assert.end();
+  });
+});
+
+test('Cache: loads the script for the first time', assert => {
+  assert.pass(scriptsNumber().length === 0);
+
+  asynz(src).then(() => {    
+    assert.pass(scriptsNumber().length === 1);    
+    assert.end();
+  });
+});
+
+test('Cache: dont load the script if was already loaded', assert => {
+  assert.pass(scriptsNumber().length === 0);
+
+  asynz(src).then(() => {    
+    assert.pass(scriptsNumber().length === 1);    
+
+    asynz(src).then(() => {
+      assert.pass(scriptsNumber().length === 1);    
+      assert.end();
+    });
+  });
+});
+
+test('Add the proper attributes to the script', assert => {
+
+});
+
+function scriptsNumber() {
+  return document.querySelectorAll('body script');
+}
 
 function test(title, cb) {
   tapeTest(title, (...args) => {
@@ -16,27 +56,3 @@ function test(title, cb) {
     cb(...args);
   });
 }
-
-test('Loading jquery', (assert) => {
-  const src = 'https://code.jquery.com/jquery-2.2.1.js';
-
-  assert.pass(!window.jQuery);
-
-  asynz(src).then(() => {
-    assert.pass(!!window.jQuery);
-
-    assert.end();
-  });
-});
-
-test('Cache: loads the script for the first time', () => {
-  console.log(window.jQuery);
-});
-
-test('Cache: dont load the script if was already loaded', () => {
-
-});
-
-test('Add the proper attributes to the script', () => {
-
-});
